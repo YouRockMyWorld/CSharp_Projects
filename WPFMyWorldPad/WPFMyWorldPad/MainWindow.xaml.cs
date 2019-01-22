174d816cc6aef2879f1a22b9b906a54e06826b47
YouRockMyWorld <sc_yourockmyworld@163.com>
2019-01-22 13:16:47 +0800

    add project.
    learn WPF.

---
+++ WPFMyWorldPad/WPFMyWorldPad/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMyWorldPad
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+            statBatText.Text = "Exit the Application";
+        }
+
+        private void MouseLeaveArea(object sender, MouseEventArgs e)
+        {
+            statBatText.Text = "Ready";
+        }
+
+        private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+        {
+            statBatText.Text = "Show Spelling Suggestions";
+        }
+
+        private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+        {
+            string spellingHints = string.Empty;
+            SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+            if(error != null)
+            {
+                foreach(string s in error.Suggestions)
+                {
+                    spellingHints += string.Format("{0}\n", s);
+                }
+                lblSpellingHints.Content = spellingHints;
+                expanderSpelling.IsExpanded = true;
+            }
+        }
+    }
+}
