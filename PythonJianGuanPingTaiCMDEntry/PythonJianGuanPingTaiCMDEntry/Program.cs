666a1882bcfb211c3b7a151dfeda752ce19df28e
YouRockMyWorld <sc_yourockmyworld@163.com>
2019-02-25 14:33:55 +0800

    Create Program.cs

---
+++ PythonJianGuanPingTaiCMDEntry/PythonJianGuanPingTaiCMDEntry/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythonJianGuanPingTaiCMDEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using(Process p =new Process())
+                {
+                    p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.CreateNoWindow = true;
+
+                    p.Start();
+                    p.StandardInput.WriteLine("python E:\\Projects\\Python_Projects\\JianCePingTai\\MainWindow.py");
+                    p.StandardInput.AutoFlush = true;
+                    p.StandardInput.WriteLine("exit");
+                    Console.Write(p.StandardOutput.ReadToEnd());
+                    //Console.Write(p.StandardError.ReadToEnd());
+                    //Console.ReadKey();
+                    //p.WaitForExit();
+                    p.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
